9af9854829fc8feaabd240157586950fdb22baa5
Michael <michael.ries@gmail.com>
2017-04-25 18:27:39 -0500

    add /locations listing

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,13 +1,13 @@
 module Slugifiable
   module InstanceMethods
     def slug
-      username.parameterize
+      name.parameterize
     end
   end
   module ClassMethods
     def find_by_slug(slug)
       # is there a better way to do this?
-      self.all.detect {|i| i.username.parameterize == slug}
+      self.all.detect {|i| i.name.parameterize == slug}
     end
   end
 end

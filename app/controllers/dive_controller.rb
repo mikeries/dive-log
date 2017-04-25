d0564a3b1325d3d410459c7d061eee1ea23d6a87
Michael <michael.ries@gmail.com>
2017-04-25 13:18:05 -0500

    create basic dives/new view.  Still needs location

--- app/controllers/dive_controller.rb
+++ app/controllers/dive_controller.rb
@@ -7,24 +7,30 @@ class DiveController < ApplicationController
   end
 
   get '/dives/new' do
+    redirect to '/' if !logged_in?
     erb :'dives/new'    
   end
 
   post '/dives' do
+    redirect to '/' if !logged_in?
   end
 
   get '/dives/:slug' do
+    redirect to '/' if !logged_in?
     erb :'dives/show'
   end
   
   post '/dives/:slug' do
+    redirect to '/' if !logged_in?  
   end
 
   get '/dives/:slug/edit' do
+    redirect to '/' if !logged_in?
     erb :'dives/edit'
   end
 
   post '/dives/:slug/delete' do
+    redirect to '/' if !logged_in? 
   end
 
 end

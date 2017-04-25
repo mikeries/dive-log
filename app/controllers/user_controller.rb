94c3973bb0373a558a89c68d3b02b0d27e6359af
Michael <michael.ries@gmail.com>
2017-04-24 19:25:02 -0500

    flesh out post/login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,8 +4,26 @@ class UserController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do
+    diver = Diver.find_by(:email => params[:email])
+  	if diver && diver.authenticate(params[:password])
+      session["diver_id"] = diver.id
+      redirect to "/divers"
+    else
+      redirect to "/login"
+    end
+  end
+  
   get '/signup' do
     erb :'users/signup'
+  end
+
+  post '/signup' do
+  end
+
+  get '/logout' do
+    session.clear if logged_in?
+    redirect to '/'
   end
 
 end

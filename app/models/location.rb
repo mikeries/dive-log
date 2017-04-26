772ca5b34b3386dd107cad59a8ef1d1f865e63a0
Michael <michael.ries@gmail.com>
2017-04-26 18:54:40 -0500

    fix has_many relationship with divers

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
   has_many :dives
-  has_many :locations, through: :dives
+  has_many :divers, through: :dives
   validates_presence_of :name, :city, :country
   validates_uniqueness_of :name
 end
